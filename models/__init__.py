7b127cf9aca9edf14069129e8c5c654484197232
dyymon <davkovar@email.cz>
2022-10-11 10:35:20 +0200

    changed headers

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,3 @@
 # Copyright (c) CTU -- All Rights Reserved
 # Created on: 11.10.2022
 #     Author: David Kovar <kovarda8@fel.cvut.cz>
-#
--- models/objects/cuboid.py
+++ models/objects/cuboid.py
@@ -3,7 +3,6 @@
 # Copyright (c) CTU -- All Rights Reserved
 # Created on: 11.10.2022
 #     Author: David Kovar <kovarda8@fel.cvut.cz>
-#
 
 import os
 import tempfile
