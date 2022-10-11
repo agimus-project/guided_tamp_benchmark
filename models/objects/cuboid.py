4cc417e122d9155a221a7fd381616122628c5469
dyymon <davkovar@email.cz>
2022-10-11 11:34:08 +0200

    changing cuboid.py

--- models/objects/cuboid.py
+++ models/objects/cuboid.py
@@ -14,21 +14,17 @@ class Cuboid(object):
     urdfSuffix = ""
     srdfSuffix = ""
 
-    def __init__(self, lengths, coords_display=False, side_handles=False) -> None:
+    def __init__(self, lengths: list[float]) -> None:
         """
         Create urdf/srdf files for the box of given size.
         This object can be passed to hpp function loadEnvironmentObject() or loadObjectModel() as argument.
 
         :param lengths: sizes of the cuboid object [width in x, width in y, width in z]
-        :param coords_display: will display coordinate system of the cuboid
-        :param side_handles: If true, additional handles which are placed at the sides of the cuboid will be added
         """
         super().__init__()
-        self.coords_disp = coords_display
         self.h1_active = False
         self.h2_active = False
         self.h3_active = False
-        self.side_handles = side_handles
         if isinstance(lengths, float):
             lengths = [lengths] * 3
         else:
@@ -40,9 +36,9 @@ class Cuboid(object):
         self.fd_srdf, self.srdfFilename = tempfile.mkstemp(suffix=".srdf", text=True)
 
         with os.fdopen(self.fd_urdf, "w") as f:
-            f.write(self.urdf(lengths=lengths, coords_disp=coords_display))
+            f.write(self.urdf(lengths=lengths))
         with os.fdopen(self.fd_srdf, "w") as f:
-            f.write(self.srdf(lengths=lengths, side_handles=side_handles))
+            f.write(self.srdf(lengths=lengths))
 
         self.lengths = lengths
 
@@ -55,18 +51,17 @@ class Cuboid(object):
         return [0.0, 0, self.lengths[2] / 2 + 0.001, ] + [0, 0, 0, 1]
 
     @staticmethod
-    def urdf(lengths: List[float], coords_disp, material: str = 'red', color_rgba: str = '1 0.2 0.2 0.8') -> str:
+    def urdf(lengths: List[float], material: str = 'red', color_rgba: str = '1 0.2 0.2 0.8') -> str:
         """
         this function generatestext for .urdf file with given parameters to create cuboid object
 
         :param lengths: sizes of the cuboid object [width in x, width in y, width in z]
-        :param coords_disp: will add to the text of .urdf file additional part whith coordinate system of the cuboid
         :param material: optional material name
         :param color_rgba: optional color of cuboid
         :return: text of .urdf file with the description of cuboid object
         """
 
-        base = f"""
+        return f"""
             <robot name="box">
 
                 <link name="base_link">
@@ -91,123 +86,14 @@ class Cuboid(object):
                         </geometry>
                     </collision>
                 </link>
-        """
-        editable = ""
-        if coords_disp:
-            editable = f"""
-                    <link name="X">
-                        <visual>
-                            <origin xyz="0 0 0" rpy="0 0 0" />
-                            <geometry>
-                                <box size="{lengths[0] + 0.01} {lengths[1]} {lengths[2]}"/>
-                            </geometry>
-                            <material name="red">
-                            <color rgba="1 0 0 1"/>
-                        </material>
-                        </visual>
-                    </link>
-                    <link name="Y">
-                        <visual>
-                            <origin xyz="0 0 0" rpy="0 0 0" />
-                            <geometry>
-                                <box size="{lengths[0]} {lengths[1] + 0.01} {lengths[2]}"/>
-                            </geometry>
-                            <material name="green">
-                            <color rgba="0 1 0 1"/>
-                        </material>
-                        </visual>
-                    </link>
-                    <link name="Z">
-                        <visual>
-                            <origin xyz="0 0 0" rpy="0 0 0" />
-                            <geometry>
-                                <box size="{lengths[0]} {lengths[1]} {lengths[2] + 0.01}"/>
-                            </geometry>
-                            <material name="blue">
-                            <color rgba="0 0 1 1"/>
-                        </material>
-                        </visual>
-                    </link>
-
-                    <joint name="X_joint" type="fixed">
-                        <origin rpy="0 0 0" xyz="0 0 0"/>
-                        <parent link="base_link"/>
-                        <child link="X"/>
-                    </joint>
-                    <joint name="Y_joint" type="fixed">
-                        <origin rpy="0 0 0" xyz="0 0 0"/>
-                        <parent link="base_link"/>
-                        <child link="Y"/>
-                    </joint>
-                    <joint name="Z_joint" type="fixed">
-                        <origin rpy="0 0 0" xyz="0 0 0"/>
-                        <parent link="base_link"/>
-                        <child link="Z"/>
-                    </joint>
-
-                    <link name="XV">
-                        <visual>
-                            <origin xyz="0.1 0 0" rpy="0 0 0" />
-                            <geometry>
-                                <box size="0.2 0.01 0.01"/>
-                            </geometry>
-                            <material name="red">
-                            <color rgba="1 0 0 1"/>
-                        </material>
-                        </visual>
-                    </link>
-                    <link name="YV">
-                        <visual>
-                            <origin xyz="0 0.1 0" rpy="0 0 0" />
-                            <geometry>
-                                <box size="0.01 0.2 0.01"/>
-                            </geometry>
-                            <material name="green">
-                            <color rgba="0 1 0 1"/>
-                        </material>
-                        </visual>
-                    </link>
-                    <link name="ZV">
-                        <visual>
-                            <origin xyz="0 0 0.1" rpy="0 0 0" />
-                            <geometry>
-                                <box size="0.01 0.01 0.2"/>
-                            </geometry>
-                            <material name="blue">
-                            <color rgba="0 0 1 1"/>
-                        </material>
-                        </visual>
-                    </link>
-
-                    <joint name="X_jointV" type="fixed">
-                        <origin rpy="0 0 0" xyz="0 0 0"/>
-                        <parent link="base_link"/>
-                        <child link="XV"/>
-                    </joint>
-                    <joint name="Y_jointV" type="fixed">
-                        <origin rpy="0 0 0" xyz="0 0 0"/>
-                        <parent link="base_link"/>
-                        <child link="YV"/>
-                    </joint>
-                    <joint name="Z_jointV" type="fixed">
-                        <origin rpy="0 0 0" xyz="0 0 0"/>
-                        <parent link="base_link"/>
-                        <child link="ZV"/>
-                    </joint>
-            """
-
-        end = """
             </robot>
             """
 
-        return base + editable + end
-
-    def srdf(self, lengths, side_handles=False) -> str:
+    def srdf(self, lengths: list[float]) -> str:
         """
         this function generates text for .srdf file with given parameters to create handles and contact surfaces
 
         :param lengths: [width in x, width in y, width in z]
-        :param side_handles: If true, additional handles which are placed at the sides of the cuboid will be added
         :return: text of .srdf file with handles and contact surfaces for the cuboid object
         """
 
@@ -250,9 +136,6 @@ class Cuboid(object):
                     <link name="base_link"/>
                   </handle>
 
-                """
-            if side_handles:
-                handles1 += f"""
                   <handle name="handleZpxSm" clearance="{lengths[0]}">
                     <position> 0 {-dist[1]} {dist[2]}   0.65328148 0.27059805 0.65328148 0.27059805</position>
                     <link name="base_link"/>
@@ -298,10 +181,7 @@ class Cuboid(object):
                     <link name="base_link"/>
                   </handle>
 
-                """
-            if side_handles:
-                handles2 += f"""
-                <handle name="handleZpySm" clearance="{lengths[1]}">
+                  <handle name="handleZpySm" clearance="{lengths[1]}">
                     <position> {-dist[0]} 0 {dist[2]}    0.65328148 -0.65328148  0.27059805  0.27059805</position>
                     <link name="base_link"/>
                   </handle>
@@ -345,10 +225,7 @@ class Cuboid(object):
                     <link name="base_link"/>
                   </handle>
 
-                """
-            if side_handles:
-                handles3 += f"""
-                <handle name="handleYpzSm" clearance="{lengths[2]}">
+                  <handle name="handleYpzSm" clearance="{lengths[2]}">
                     <position> {-dist[0]} {dist[1]} 0  0  0.9238795 -0.3826834 0</position>
                     <link name="base_link"/>
                   </handle>
@@ -383,7 +260,39 @@ class Cuboid(object):
 
         return preinfo + handles1 + handles2 + handles3 + afterinfo
 
-    def handles(self, name):
+    @staticmethod
+    def handles(prefix: str = ""):
+        """
+        This function returns list of curently active handles.
+        Handle description following:
+                    HandleAbc(Sd)
+                        A - X/Y/Z from which coordinate will the gripper come from
+
+                        b - m/p if the approach is from minus (m) or plus (p)
+
+                        c - x/y/z is coordinate in which the width of the grip is represented
+
+                        S - optional S means that the handle is a side handle
+
+                        d - m/p argument tells if the side handle is on minus or plus side of cuboid on the third axis
+
+        Warning: if there is unreachable handle, constraint graph validation will be incorrect!
+
+        :param prefix: prefix for handle name
+        :return: list of handles [prefix + handleAbc, prefix + handleAbc, ...]
+        """
+
+        handles = ["handleZpx", "handleZmx", "handleYmx", "handleYpx", "handleZpxSm", "handleZmxSm", "handleZpxSp",
+                   "handleZmxSp", "handleZpy", "handleZmy", "handleXmy", "handleXpy", "handleZpySm", "handleZmySm",
+                   "handleZpySp", "handleZmySp", "handleYpz", "handleYmz", "handleXmz", "handleXpz", "handleYpzSm",
+                   "handleYmzSm", "handleYpzSp", "handleYmzSp"]
+
+        for handle in handles:
+            handle = prefix + handle
+
+        return handles
+
+    def active_handles(self, name):
         """
         This function returns list of curently active handles.
         Handle description following:
@@ -408,33 +317,27 @@ class Cuboid(object):
 
         if self.h1_active:
             handle_list = handle_list + [name + "/handleZpx", name + "/handleZmx", name + "/handleYmx",
-                                         name + "/handleYpx"]
-            if self.side_handles:
-                handle_list += [name + "/handleZpxSm", name + "/handleZmxSm", name + "/handleZpxSp",
-                                name + "/handleZmxSp"]
+                                         name + "/handleYpx", name + "/handleZpxSm", name + "/handleZmxSm",
+                                         name + "/handleZpxSp", name + "/handleZmxSp"]
         if self.h2_active:
             handle_list = handle_list + [name + "/handleZpy", name + "/handleZmy", name + "/handleXmy",
-                                         name + "/handleXpy"]
-            if self.side_handles:
-                handle_list += [name + "/handleZpySm", name + "/handleZmySm", name + "/handleZpySp",
-                                name + "/handleZmySp"]
+                                         name + "/handleXpy", name + "/handleZpySm", name + "/handleZmySm",
+                                         name + "/handleZpySp", name + "/handleZmySp"]
         if self.h3_active:
             handle_list = handle_list + [name + "/handleYpz", name + "/handleYmz", name + "/handleXmz",
-                                         name + "/handleXpz"]
-            if self.side_handles:
-                handle_list += [name + "/handleYpzSm", name + "/handleYmzSm", name + "/handleYpzSp",
-                                name + "/handleYmzSp"]
+                                         name + "/handleXpz", name + "/handleYpzSm", name + "/handleYmzSm",
+                                         name + "/handleYpzSp", name + "/handleYmzSp"]
 
         return handle_list
 
-    def contact_surfaces(self, name):
+    def contact_surfaces(self, prefix: str = ""):
         """
         This function returns the name of the contact surface of the object
 
-        :param name: argument which is the name of the handled object passed to HPP corbaserver
-        :return: name as string "name/box_surface"
+        :param prefix: prefix for contact surface name
+        :return: name as string prefix + "box_surface"
         """
-        surface = name + "/box_surface"
+        surface = prefix + "box_surface"
         return surface
 
     def __del__(self):
