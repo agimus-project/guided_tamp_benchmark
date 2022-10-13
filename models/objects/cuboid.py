5ccf0e6f0aaaea170aa897bad408d8f2615bf698
Vladimir Petrik <vladimir.petrik@cvut.cz>
2022-10-13 14:15:37 +0200

    cuboid remove handles disabling and add base class for objects.

--- models/objects/cuboid.py
+++ models/objects/cuboid.py
@@ -1,43 +1,42 @@
 #!/usr/bin/env python
-import copy
+
 # Copyright (c) CTU -- All Rights Reserved
 # Created on: 11.10.2022
 #     Author: David Kovar <kovarda8@fel.cvut.cz>
 
 import os
 import tempfile
-from typing import List
+from typing import List, Union
+from .base import BaseObject
 
 
-class Cuboid(object):
+class Cuboid(BaseObject):
     rootJointType = "freeflyer"
     urdfSuffix = ""
     srdfSuffix = ""
 
-    handles = ["handleZpx", "handleZmx", "handleYmx", "handleYpx", "handleZpxSm", "handleZmxSm", "handleZpxSp",
-               "handleZmxSp", "handleZpy", "handleZmy", "handleXmy", "handleXpy", "handleZpySm", "handleZmySm",
-               "handleZpySp", "handleZmySp", "handleYpz", "handleYmz", "handleXmz", "handleXpz", "handleYpzSm",
-               "handleYmzSm", "handleYpzSp", "handleYmzSp"]
+    _all_handles = ["handleZpx", "handleZmx", "handleYmx", "handleYpx", "handleZpxSm", "handleZmxSm", "handleZpxSp",
+                    "handleZmxSp", "handleZpy", "handleZmy", "handleXmy", "handleXpy", "handleZpySm", "handleZmySm",
+                    "handleZpySp", "handleZmySp", "handleYpz", "handleYmz", "handleXmz", "handleXpz", "handleYpzSm",
+                    "handleYmzSm", "handleYpzSp", "handleYmzSp"]
 
-    def __init__(self, lengths: List[float], disabled_handles: List[int], grasp_depth: float) -> None:
+    def __init__(self, lengths: Union[List[float], float], max_handles_depth: float = 0.03) -> None:
         """
-        Create urdf/srdf files for the box of given size.
-        This object can be passed to hpp function loadEnvironmentObject() or loadObjectModel() as argument.
+        Create urdf/srdf files for the box of given size. This object can be passed to hpp function
+        loadEnvironmentObject() or loadObjectModel() as argument.
 
         :param lengths: sizes of the cuboid object [width in x, width in y, width in z] or single float for symmetric
         cube
-        :param disabled_handles: list of IDs of the handles to be disabled
-        :param grasp_depth:
+        :param max_handles_depth: maximum distance of the handles from the respective object surface;
+            if the object is small, handle is in the center of the object
+            if the object is big, the handle will not penetrate object more than 'max_handle_depth' distance
+            For example, it can be set to be equal to length of the finger of the gripper.
         """
         super().__init__()
         if isinstance(lengths, float):
             lengths = [lengths] * 3
-        else:
-            pass
-
-        self.disabled_handles = disabled_handles
-
         assert len(lengths) == 3
+        self.lengths = lengths
 
         self.fd_urdf, self.urdfFilename = tempfile.mkstemp(suffix=".urdf", text=True)
         self.fd_srdf, self.srdfFilename = tempfile.mkstemp(suffix=".srdf", text=True)
@@ -45,9 +44,7 @@ class Cuboid(object):
         with os.fdopen(self.fd_urdf, "w") as f:
             f.write(self.urdf(lengths=lengths))
         with os.fdopen(self.fd_srdf, "w") as f:
-            f.write(self.srdf(lengths=lengths, grasp_depth=grasp_depth))
-
-        self.lengths = lengths
+            f.write(self.srdf(lengths=lengths, max_handles_depth=max_handles_depth))
 
     def initial_configuration(self) -> List[float]:
         """
@@ -56,6 +53,40 @@ class Cuboid(object):
         :return: initial configuration of cuboid considering its size
         """
         return [0.0, 0, self.lengths[2] / 2 + 0.001, ] + [0, 0, 0, 1]
+
+    @classmethod
+    def handles(cls, prefix: str = ""):
+        """
+        This function returns list of all handles prepended with the prefix.
+        Handle description following:
+                    HandleAbc(Sd)
+                        A - X/Y/Z from which coordinate will the gripper come from
+
+                        b - m/p if the approach is from minus (m) or plus (p)
+
+                        c - x/y/z is coordinate in which the width of the grip is represented
+
+                        S - optional S means that the handle is a side handle
+
+                        d - m/p argument tells if the side handle is on minus or plus side of cuboid on the third axis
+
+        :param prefix: prefix for handle name
+        :return: list of handles [prefix + handleAbc, prefix + handleAbc, ...]
+        """
+        return [prefix + h for h in cls._all_handles]
+
+    @classmethod
+    def contact_surfaces(cls, prefix: str = ""):
+        """
+        This function returns the list of all contact surface defined by the object.
+        :param prefix: prefix for contact surface name
+        :return: name as string prefix + "box_surface"
+        """
+        return [prefix + "box_surface"]
+
+    def __del__(self):
+        os.unlink(self.urdfFilename)
+        os.unlink(self.srdfFilename)
 
     @staticmethod
     def urdf(lengths: List[float], material: str = 'red', color_rgba: str = '1 0.2 0.2 0.8') -> str:
@@ -97,18 +128,17 @@ class Cuboid(object):
             """
 
     @staticmethod
-    def srdf(lengths: List[float], gripper_depth: float) -> str:
+    def srdf(lengths: List[float], max_handles_depth: float) -> str:
         """
         this function generates text for .srdf file with given parameters to create handles and contact surfaces
 
         :param lengths: [width in x, width in y, width in z]
+        :param max_handles_depth: the maximum distance of handles from the object boundary
         :return: text of .srdf file with handles and contact surfaces for the cuboid object
         """
 
-        dist = [0, 0, 0]  # is the distance of the grip from the center of the cuboid
-        for i, length in enumerate(lengths):
-            if length > gripper_depth:
-                dist[i] = abs(gripper_depth - length) / 2
+        # is the distance of the handle from the center of the cuboid
+        dist = [0 if l / 2 < max_handles_depth else l / 2 - max_handles_depth for l in lengths]
         a, b, c = [l / 2 for l in lengths]
 
         return f"""
@@ -245,70 +275,3 @@ class Cuboid(object):
                   </contact>
                 </robot>
                """
-
-
-    def handles(self, prefix: str = ""):
-        """
-        This function returns list of curently active handles.
-        Handle description following:
-                    HandleAbc(Sd)
-                        A - X/Y/Z from which coordinate will the gripper come from
-
-                        b - m/p if the approach is from minus (m) or plus (p)
-
-                        c - x/y/z is coordinate in which the width of the grip is represented
-
-                        S - optional S means that the handle is a side handle
-
-                        d - m/p argument tells if the side handle is on minus or plus side of cuboid on the third axis
-
-        Warning: if there is unreachable handle, constraint graph validation will be incorrect!
-
-        :param prefix: prefix for handle name
-        :return: list of handles [prefix + handleAbc, prefix + handleAbc, ...]
-        """
-
-        return [prefix + h for h in self.handles]
-
-    def active_handles(self, prefix: str = ""):
-        """
-        This function returns list of curently active handles.
-        Handle description following:
-                    HandleAbc(Sd)
-                        A - X/Y/Z from which coordinate will the gripper come from
-
-                        b - m/p if the approach is from minus (m) or plus (p)
-
-                        c - x/y/z is coordinate in which the width of the grip is represented
-
-                        S - optional S means that the handle is a side handle
-
-                        d - m/p argument tells if the side handle is on minus or plus side of cuboid on the third axis
-
-        Warning: if there is unreachable handle, constraint graph validation will be incorrect!
-
-        :param prefix: prefix for handle name
-        :return: list of handles [prefix + handleAbc, prefix + handleAbc, ...]
-        """
-
-        active_handles = copy.deepcopy(self.handles)
-
-        for d in self.disabled_handles:
-            active_handles.pop(d)
-
-        return active_handles
-
-    @staticmethod
-    def contact_surfaces(prefix: str = ""):
-        """
-        This function returns the name of the contact surface of the object
-
-        :param prefix: prefix for contact surface name
-        :return: name as string prefix + "box_surface"
-        """
-        surface = prefix + "box_surface"
-        return surface
-
-    def __del__(self):
-        os.unlink(self.urdfFilename)
-        os.unlink(self.srdfFilename)
