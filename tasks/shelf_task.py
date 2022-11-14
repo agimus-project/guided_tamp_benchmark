67dcb65d1683b0ec2e9e6cbb2eab6154cecaf3e0
dyymon <davkovar@email.cz>
2022-11-14 16:44:24 +0100

    adding first task

---
+++ tasks/shelf_task.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Copyright (c) CTU -- All Rights Reserved
+# Created on: 14.11.22
+#     Author: David Kovar <kovarda8@fel.cvut.cz>
+
+class ShelfTask():
+
+    def __init__(self, ):
+        pass
