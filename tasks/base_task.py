1a9ddc05f3f39bfe9aa160b77d8222a657dcc850
dyymon <davkovar@email.cz>
2022-11-15 11:02:34 +0100

    base_task first commit

---
+++ tasks/base_task.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Copyright (c) CTU -- All Rights Reserved
+# Created on: 15.11.22
+#     Author: David Kovar <kovarda8@fel.cvut.cz>
+
+class Task:
+    def __init__(self, demo, robot):
+        pass
+
+    def check_collision(self):
+        pass
+
+    def check_grasp_constraint(self):
+        pass
+
+    def check_place_constraint(self):
+        pass
+
+    def compute_lenght(self):
+        pass
+
+    def is_sucessful(self):
+        pass
+
+    def compute_n_grasps(config_list):
+        pass
