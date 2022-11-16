4395f803f838db5d193da56e08cb23f885e5eab5
dyymon <davkovar@email.cz>
2022-11-16 13:18:52 +0100

    remade load function

--- tasks/demonstration.py
+++ tasks/demonstration.py
@@ -21,7 +21,7 @@ class Demonstration:
         self.object_ids: Optional[List[str]] = None  # can be a list of e.g. YCBV_01 or CUBOID_0.1_0.2_0.8
         # todo: add other types as well
         self.objects_poses = None
-        self.contacts = None
+        self.contacts: Optional[List[List[int]]] = None
         self.robot_pose = None
         self.furniture_ids = None
         self.furniture_poses = None
@@ -35,18 +35,19 @@ class Demonstration:
     def get_robot_pose(self):
         return self.robot_pose
 
-    def load(self, task_name, demo_file, robot_name, pose_id):
-        # todo: this should be a static function
-        # todo: demo_file --> demo_id
-        with open(demo_file, 'rb') as f:
+    @staticmethod
+    def load(task_name, demo_id, robot_name, pose_id):
+        with open("data/" + task_name + "_" + demo_id + ".pkl", 'rb') as f:
             data = pickle.load(f)
-        self.object_ids = data["object_ids"]
-        self.objects_poses = data["objects_poses"]
-        self.contacts = data["contacts"]
-        self.robot_pose = data["robot_pose"][robot_name][pose_id]
-        self.furniture_ids = data["furniture_ids"]
-        self.furniture_poses = data["furniture_poses"]
-        return self
+        Demonstration.object_ids = data["object_ids"]
+        Demonstration.objects_poses = data["objects_poses"]
+        Demonstration.contacts = data["contacts"]
+        Demonstration.furniture_ids = data["furniture_ids"]
+        Demonstration.furniture_poses = data["furniture_poses"]
+        with open("data/" + task_name + "_" + demo_id + "_" + robot_name + "_poses.pkl", 'rb') as f:
+            robot_data = pickle.load(f)
+        Demonstration.robot_pose = robot_data[pose_id]
+        return Demonstration
 
     def save(self, demo_file, robot_name, pose_id):
         with open(demo_file, 'rb') as f:
