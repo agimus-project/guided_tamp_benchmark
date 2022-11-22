a8fbb84327d8a772764661e47a2b6b328e2d5eed
dyymon <davkovar@email.cz>
2022-11-22 15:11:42 +0100

    updated demonstration.py

--- tasks/demonstration.py
+++ tasks/demonstration.py
@@ -4,7 +4,7 @@
 # Created on: 14.11.22
 #     Author: David Kovar <kovarda8@fel.cvut.cz>
 
-
+import os
 import pickle
 from typing import Optional, List
 
@@ -18,9 +18,9 @@ class Demonstration:
         self.robot_name: Optional[str] = None
         self.pose_id: Optional[str] = None
         self.object_ids: Optional[List[str]] = None  # can be a list of e.g. YCBV_01 or CUBOID_0.1_0.2_0.8
-        self.objects_poses: Optional[np.array] = None # n*t*4x4 numpy array
+        self.objects_poses: Optional[np.array] = None  # n*t*4x4 numpy array
         self.contacts: Optional[List[List[int]]] = None
-        self.robot_pose: Optional[np.array] = None # 4x4 numpy array
+        self.robot_pose: Optional[np.array] = None  # 4x4 numpy array
         self.furniture_ids: Optional[List[str]] = None
         self.furniture_poses: Optional[np.array] = None
 
@@ -53,53 +53,61 @@ class Demonstration:
         return demo
 
     def save(self, demo_file, robot_name, pose_id):
-        demo = dict(object_ids=[], object_poses=[], contacts=[], furniture_ids=[], furniture_poses=[])
-        demo["object_ids"] = self.object_ids
-        demo["objects_poses"] = self.objects_poses
-        demo["contacts"] = self.contacts
-        demo["furniture_ids"] = self.furniture_ids
-        demo["furniture_poses"] = self.furniture_poses
-        with open("data/" + demo_file + '.pkl', 'wb') as handle:
-            pickle.dump(demo, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        robot = dict(pose_id=[])
-        robot[pose_id] = self.robot_pose
-        with open("data/" + demo_file + "_" + robot_name + '_poses.pkl', 'wb') as handle:
-            pickle.dump(robot, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        if os.path.exists("data/" + demo_file + '.pkl'):
+            with open("data/" + demo_file + "_" + robot_name + '_poses.pkl', "rb") as f:
+                robot = pickle.load(f)
+            assert pose_id not in robot, f"{pose_id} is already a pose_id in {demo_file}_{robot_name}_poses.pkl"
+            robot[pose_id] = self.robot_pose
+            with open("file.txt", "wb") as f:
+                pickle.dump(robot, f)
+        else:
+            demo = dict(object_ids=[], object_poses=[], contacts=[], furniture_ids=[], furniture_poses=[])
+            demo["object_ids"] = self.object_ids
+            demo["objects_poses"] = self.objects_poses
+            demo["contacts"] = self.contacts
+            demo["furniture_ids"] = self.furniture_ids
+            demo["furniture_poses"] = self.furniture_poses
+            with open("data/" + demo_file + '.pkl', 'wb') as handle:
+                pickle.dump(demo, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            robot = dict(pose_id=[])
+            robot[pose_id] = self.robot_pose
+            with open("data/" + demo_file + "_" + robot_name + '_poses.pkl', 'wb') as handle:
+                pickle.dump(robot, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
-    a = [np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.031], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.031], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.041], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.051], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.061], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.071], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.081], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.14], [0., 0., 1., 0.081], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.13], [0., 0., 1., 0.081], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.12], [0., 0., 1., 0.081], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.11], [0., 0., 1., 0.081], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.081], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.071], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.061], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.051], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.041], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.031], [0., 0., 0., 1.]]),
-         np.array([[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.031], [0., 0., 0., 1.]])]
+    a = np.array([[[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.031], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.031], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.041], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.051], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.061], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.071], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.15], [0., 0., 1., 0.081], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.14], [0., 0., 1., 0.081], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.13], [0., 0., 1., 0.081], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.12], [0., 0., 1., 0.081], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.11], [0., 0., 1., 0.081], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.081], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.071], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.061], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.051], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.041], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.031], [0., 0., 0., 1.]],
+                  [[1., 0., 0., 0.15], [0., 1., 0., 0.10], [0., 0., 1., 0.031], [0., 0., 0., 1.]]])
     b = [0] + 16 * [1] + [0]
 
-    c = [np.array([[1., 0., 0., 0.15], [0., 1., 0., -0.15], [0., 0., 1., 0.031], [0., 0., 0., 1.]])] * 18
+    c = np.array([[[1., 0., 0., 0.15], [0., 1., 0., -0.15], [0., 0., 1., 0.031], [0., 0., 0., 1.]]] * 18)
     d = [0] * 18
 
-    dummy = {"object_ids": ["cuboid_0.06_0.06_0.06", "cuboid_0.06_0.06_0.06"], "objects_poses": [a, c],
-             "contacts": [b, d], "furniture_ids": ["table"], "furniture_poses": [np.eye(4)]}
-    robot_dummy = {"robot_pose": [np.eye(4)]}
+    dummy = {"object_ids": ["cuboid_0.06_0.06_0.06", "cuboid_0.06_0.06_0.06"], "objects_poses": np.array([a, c]),
+             "contacts": [b, d], "furniture_ids": ["table"], "furniture_poses": np.array([np.eye(4)])}
+    robot_dummy = {"robot_pose_0": np.eye(4)}
 
     with open('data/dummy_0.pkl', 'wb') as handle:
         pickle.dump(dummy, handle, protocol=pickle.HIGHEST_PROTOCOL)
     with open('data/dummy_0_panda_robot_poses.pkl', 'wb') as handle:
         pickle.dump(robot_dummy, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    demo = Demonstration.load('dummy', 0, "panda_robot", "robot_pose")
-    demo.save('shelf_1', "panda_robot", "robot_pose")
-    demo2 = Demonstration.load("shelf", 1, "panda_robot", "robot_pose")
+    demo = Demonstration.load('dummy', 0, "panda_robot", "robot_pose_0")
+    demo.save('shelf_0', "panda_robot", "robot_pose_0")
+    demo2 = Demonstration.load("shelf", 0, "panda_robot", "robot_pose_0")
