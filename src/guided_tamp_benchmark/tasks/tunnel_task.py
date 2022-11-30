5d27dcab4fe6d63b472462c69d391a8b907877f1
David Kovář <92931534+Dyymon@users.noreply.github.com>
2022-11-30 10:45:48 +0100

    adresing comments

--- src/guided_tamp_benchmark/tasks/tunnel_task.py
+++ src/guided_tamp_benchmark/tasks/tunnel_task.py
@@ -10,4 +10,4 @@ from guided_tamp_benchmark.models.robots import BaseRobot
 
 class TunnelTask(BaseTask):
     def __init__(self, demo_id: int, robot: BaseRobot, robot_pose_id: int):
-        super().__init__("Tunnel", demo_id=demo_id, robot=robot, robot_pose_id=robot_pose_id)
+        super().__init__("tunnel", demo_id=demo_id, robot=robot, robot_pose_id=robot_pose_id)
