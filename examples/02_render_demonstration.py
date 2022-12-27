2a8516f8e6d5fd41a3ceb66bf1110ae54c481540
kzorina <ekaterina.zorina95@gmail.com>
2022-12-27 11:02:32 +0100

    Clean up render demonstration example

--- examples/02_render_demonstration.py
+++ examples/02_render_demonstration.py
@@ -6,30 +6,12 @@
 #
 
 import time
-import numpy as np
-import pinocchio as pin
 from guided_tamp_benchmark.models.robots import *
-from guided_tamp_benchmark.models.furniture import *
-from guided_tamp_benchmark.tasks.base_task import BaseTask
-from guided_tamp_benchmark.tasks.shelf_task import ShelfTask
+from guided_tamp_benchmark.tasks import *
 from guided_tamp_benchmark.tasks.renderer import Renderer
 
-
-# todo: change to one of the existing demonstrations
-class TestTask(BaseTask):
-
-    def __init__(self):
-        super().__init__('test', 0, PandaRobot(), 0)
-
-task = TestTask()
-# task = ShelfTask(0, PandaRobot(), 0)
-# task.furniture.clear()
-# f = pin.SE3(np.eye(4))
-# f.translation[0] = 1.
-# task.furniture.append(Table(pose=f.homogeneous, desk_size=[1., 1., 0.7]))
-# task.furniture.append(Shelf(position=[-1, 0, 2], rpy=[0, 0, 0], display_inside_shelf=True))
-# task.furniture.append(Tunnel(position=[0, 2, 0], rpy=[0, 0, 0], lengths=[1, 1, 1]))
-
+# demo_id, robot, pose_id
+task = TunnelTask(0, PandaRobot(), 0)
 r = Renderer(task=task)
 r.animate_demonstration(fps=1)
 
