9a8a273413928fcf16850d7efe8866c2790f3f53
dyymon <davkovar@email.cz>
2023-02-22 14:15:19 +0100

    collisions.py

--- collisions.py
+++ collisions.py
@@ -6,11 +6,10 @@
 
 from __future__ import print_function
 import pinocchio as pin
-import pickle
+from pinocchio.visualize import MeshcatVisualizer
 import sys
 
 import numpy as np
-from os.path import dirname, join, abspath
 
 from guided_tamp_benchmark.models.utils import get_models_data_directory
 from guided_tamp_benchmark.models.robots.panda_robot import PandaRobot
@@ -20,13 +19,6 @@ from guided_tamp_benchmark.models.objects.object_ycbv import ObjectYCBV
 
 from guided_tamp_benchmark.tasks.shelf_task import ShelfTask
 
-try:
-    import hppfcl
-except ImportError:
-    print("This example requires hppfcl")
-    sys.exit(0)
-from pinocchio.visualize import MeshcatVisualizer
-
 
 def rename_frames(model, model_name):
     for i, frame in enumerate(model.frames):
@@ -34,15 +26,17 @@ def rename_frames(model, model_name):
             continue
         frame.name = f"{model_name}{i}"
 
+
 def rename_joints(model, model_name):
     for i in range(0, len(model.names)):
         if i == 0:
             continue
         model.names[i] = f"{model_name}{i}"
 
+
 def create_model(robot, objects, furniture):
     p_r, c_r, _ = pin.buildModelsFromUrdf(robot.urdfFilename, package_dirs=str(get_models_data_directory()),
-                                      root_joint=pin.JointModelFreeFlyer())
+                                          root_joint=pin.JointModelFreeFlyer())
 
     for i, model in enumerate(objects + furniture):
         if i < len(objects):
@@ -51,7 +45,7 @@ def create_model(robot, objects, furniture):
             # rename_joints(p, model.name)
             rename_frames(p, model.name)
             p.names[1] = f"{model.name}{i}_root_joint"
-            p_r, c_r = pin.appendModel(p_r, p, c_r, c, 0,pin.SE3(np.eye(3), np.array([0, 0, 0])))
+            p_r, c_r = pin.appendModel(p_r, p, c_r, c, 0, pin.SE3(np.eye(3), np.array([0, 0, 0])))
         else:
             p, c, _ = pin.buildModelsFromUrdf(model.urdfFilename, package_dirs=str(get_models_data_directory()))
             rename_frames(p, model.name)
@@ -62,25 +56,6 @@ def create_model(robot, objects, furniture):
     pin.removeCollisionPairs(p_r, c_r, robot.srdfFilename)
     return p_r, c_r
 
-
-def vizulize_through_pinocchio(pin_mod, col_mod, q):
-    viz = MeshcatVisualizer(pin_mod, col_mod, col_mod)
-
-    # Initialize the viewer.
-    try:
-        viz.initViewer(open=True)
-    except ImportError as error:
-        print(error)
-        sys.exit(0)
-
-    try:
-        viz.loadViewerModel("shapes")
-    except AttributeError as error:
-        print(error)
-        sys.exit(0)
-
-    viz.display(q)
-    input("press enter to continue")
 
 def extract_from_task(task):
     furniture = []
@@ -95,27 +70,70 @@ def extract_from_task(task):
 
     robot = task.robot
 
-    return {"robot": robot, "objects": objects, "furniture":furniture}
+    return {"robot": robot, "objects": objects, "furniture": furniture}
 
 
-task = ShelfTask(demo_id = 0, robot = PandaRobot(), robot_pose_id = 1)
-pin_mod, col_mod = create_model(**extract_from_task(task))
+def pose_as_matrix_to_pose_as_quat(pose):
+    T = pin.SE3(pose[:3, :3], np.squeeze(pose[:3, 3:]))
+    xyz_quat = pin.se3ToXYZQUAT(T)
+    # quat = list(R.from_matrix(task.demo.objects_poses[:3, :3]).as_quat())
+    # t = list(np.squeeze(task.demo.objects_poses[:3, 3:]))
+    return xyz_quat
 
-q = np.array([0.4,0.3,0,0,0,0,1] + [0, 0, 0, 0, 0, 0, 1] + [0, -np.pi / 4, 0, -3 * np.pi / 4, 0, np.pi / 2, np.pi / 4, 0.,
-                                      0.])
 
-# Create data structures
-data = pin_mod.createData()
-geom_data = pin.GeometryData(col_mod)
+class Collision:
+    def __init__(self, task):
+        self.pin_mod, self.col_mod = create_model(**extract_from_task(task))
+        self.task = task
+        self.robot_pose = pose_as_matrix_to_pose_as_quat(task.get_robot_pose())
 
-# Compute all the collisions
-print(pin.computeCollisions(pin_mod, data, col_mod, geom_data, q, False))
+    def is_config_valid(self, config):
 
-# Print the status of collision for all collision pairs
-for k in range(len(col_mod.collisionPairs)):
-    cr = geom_data.collisionResults[k]
-    cp = col_mod.collisionPairs[k]
-    print("collision pair:", cp.first, ",", cp.second, "- collision:", "Yes" if cr.isCollision() else "No")
+        config = np.array(config[:-len(self.task.robot.initial_configuration())] + list(self.robot_pose) + \
+                 config[-len(self.task.robot.initial_configuration()):])
 
-vizulize_through_pinocchio(pin_mod, col_mod, q)
+        # Create data structures
+        data = self.pin_mod.createData()
+        geom_data = pin.GeometryData(self.col_mod)
+
+        # Compute all the collisions
+        print(pin.computeCollisions(self.pin_mod, data, self.col_mod, geom_data, config, False))
+
+        for k in range(len(self.col_mod.collisionPairs)):
+            cr = geom_data.collisionResults[k]
+            cp = self.col_mod.collisionPairs[k]
+            print("collision pair:", cp.first, ",", cp.second, "- collision:", "Yes" if cr.isCollision() else "No")
+
+    def vizulize_through_pinocchio(self, config):
+
+        config = np.array(config[:-len(self.task.robot.initial_configuration())] + list(self.robot_pose) + \
+                          config[-len(self.task.robot.initial_configuration()):])
+
+        viz = MeshcatVisualizer(self.pin_mod, self.col_mod, self.col_mod)
+
+        # Initialize the viewer.
+        try:
+            viz.initViewer(open=True)
+        except ImportError as error:
+            print(error)
+            sys.exit(0)
+
+        try:
+            viz.loadViewerModel("shapes")
+        except AttributeError as error:
+            print(error)
+            sys.exit(0)
+
+        viz.display(config)
+        input("press enter to continue")
+
+
+task = ShelfTask(demo_id=0, robot=PandaRobot(), robot_pose_id=1)
+
+collision = Collision(task)
+
+config = [0.4, 0.3, 0, 0, 0, 0, 1] + [0, -np.pi / 4, 0, -3 * np.pi / 4, 0, np.pi / 2, np.pi / 4, 0., 0.]
+
+collision.is_config_valid(config)
+collision.vizulize_through_pinocchio(config)
 pass
