b1ad9526dc2b2f204b732418d9ee0ecc1e102c1f
dyymon <davkovar@email.cz>
2023-03-14 09:39:40 +0100

    initial commit

--- src/guided_tamp_benchmark/utilities/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# Copyright (c) CTU -- All Rights Reserved
-# Created on: 13.03.23
-#     Author: David Kovar <kovarda8@fel.cvut.cz>
-
-import xml.etree.ElementTree as ET
-
-pass
