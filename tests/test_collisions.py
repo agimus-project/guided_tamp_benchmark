076187371d8ba0fb9895388878ec78ac1728f220
dyymon <davkovar@email.cz>
2023-05-29 12:06:23 +0200

    removing test valid path

--- tests/test_collisions.py
+++ tests/test_collisions.py
@@ -159,40 +159,6 @@ class CollisionFunctionsTestCase(unittest.TestCase):
             True,
         )
 
-    def test_valid_path(self):
-        """test path_is_successful function"""
-        path = pathlib.Path(__file__).parent.joinpath("test_configs.pkl")
-        data = pickle.load(open(path, "rb"))
-
-        res = WaiterTask(0, KukaMobileIIWARobot(), 1).path_is_successful(
-            data["waiter_0_kmr_0"]["configs"])
-        self.assertEqual(res, (False,
-                               "last configuration of robot kmr_iiwa doesn't"
-                               " match with its goal configuration"),
-                         msg=res[1] + " tunnel task test")
-
-        res = TunnelTask(0, UR5Robot(), 1).path_is_successful(
-            data["tunnel_0_ur_1"]["configs"])
-        self.assertEqual(res[0], True, msg=res[1] + " tunnel task test")
-
-        res = ShelfTask(1, PandaRobot(), 1).path_is_successful(
-            data["shelf_1_panda_1"]["configs"])
-        self.assertEqual(res, (False,
-                               "last pose of object obj_000002 doesn't match with its "
-                               "goal configuration from demonstration"),
-                         msg=res[1] + " shelf task 2 test")
-
-        st = ShelfTask(2, PandaRobot(), 1)
-        res = st.path_is_successful([Configuration(st.robot.initial_configuration(),
-                                                   st.demo.subgoal_objects_poses[:, 0]),
-                                     Configuration(st.robot.initial_configuration(),
-                                                   st.demo.subgoal_objects_poses[:,
-                                                   -1])])
-        self.assertEqual(res, (False, 'object obj_000002, moved between configuration '
-                                      '1 and 0, even though its under placement'
-                                      ' constraint.'),
-                         msg=res[1] + " shelf task 3 test")
-
 
 if __name__ == "__main__":
     unittest.main()
