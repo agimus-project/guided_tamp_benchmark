1ea1f8d789200930a9ff878c5807ccf1aada246f
Vladimír Petrík <vladimir.petrik@cvut.cz>
2023-06-28 17:10:08 +0200

    Saving benchmark fixes (#33)
    * added test that shows saving the benchmark fails
    
    * switch saving to dill, add loading and dataclass for the result
    
    * rename scripts to benchmrak

--- guided_tamp_benchmark/benchmark/__init__.py
+++ guided_tamp_benchmark/benchmark/__init__.py
@@ -3,3 +3,6 @@
 # Copyright (c) CTU -- All Rights Reserved
 # Created on: 04.04.23
 #     Author: David Kovar <kovarda8@fel.cvut.cz>
+
+from .benchmark_result import BenchmarkResult
+from .benchmark import Benchmark
---
+++ tests/test_benchmark.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright (c) CTU -- All Rights Reserved
+# Created on: 2023-06-28
+#     Author: Vladimir Petrik <vladimir.petrik@cvut.cz>
+#
+import unittest
+from pathlib import Path
+
+from guided_tamp_benchmark.benchmark import Benchmark, BenchmarkResult
+
+
+class TestBenchmarkScript(unittest.TestCase):
+    def test_saving(self):
+        path = Path("/tmp/results.pkl")
+        b = Benchmark()
+        b.save_benchmark(path)
+        self.assertTrue(path.exists())
+
+    def test_save_load(self):
+        path = Path("/tmp/results1.pkl")
+        b = Benchmark()
+        b.results["planner"]["shelf1"][0]["panda"][0][0] = BenchmarkResult(
+            True, 10.0, (1, 2, 3.0), list(), 10
+        )
+        b.save_benchmark(path)
+        self.assertTrue(path.exists())
+        del b
+        b = Benchmark()
+        b.load_benchmark(path)
+        self.assertTrue("planner" in b.results)
+        self.assertTrue("shelf1" in b.results["planner"])
+        res: BenchmarkResult = b.results["planner"]["shelf1"][0]["panda"][0][0]
+        self.assertTrue(res.is_solved)
+        self.assertAlmostEqual(res.computation_time, 10.0)
+        self.assertAlmostEqual(res.path_len[0], 1)
+        self.assertAlmostEqual(res.path_len[1], 2)
+        self.assertAlmostEqual(res.path_len[2], 3)
+        self.assertEqual(res.subsampled_path, list())
+        self.assertEqual(res.number_of_grasps, 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
