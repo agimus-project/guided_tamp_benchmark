ca0df7fd7c314004e0478c6904220971bb1900c0
Vladimir Petrik <vladimir.petrik@cvut.cz>
2023-06-29 12:09:21 +0200

    fixing the pose id range to be 10; use render_config function

--- examples/04_visualize_robot_poses.py
+++ examples/04_visualize_robot_poses.py
@@ -13,6 +13,7 @@ from guided_tamp_benchmark.models.robots import *
 from guided_tamp_benchmark.tasks import *
 from guided_tamp_benchmark.tasks.renderer import Renderer
 
+r = None
 
 for task_cls in [TunnelTask, ShelfTask]:
     for robot_cls in [PandaRobot, UR5Robot, KukaIIWARobot]:
@@ -21,27 +22,25 @@ for task_cls in [TunnelTask, ShelfTask]:
         )
         output_folder.mkdir(parents=True, exist_ok=True)
 
-        r = None
-        for demo_id in range(0, 100):
-            for robot_pose_id in range(0, 100):
+        for demo_id in range(0, 10):
+            for robot_pose_id in range(0, 10):
                 try:
                     task = task_cls(
-                        demo_id=demo_id, robot=PandaRobot(), robot_pose_id=robot_pose_id
+                        demo_id=demo_id, robot=robot_cls(), robot_pose_id=robot_pose_id
                     )
-                except FileNotFoundError:
+                except FileNotFoundError as e:
+                    print(e)
                     continue
                 print(f"DemoId: {demo_id}, PoseId: {robot_pose_id}")
                 if r is not None:
                     r.scene.clear()
                 r = Renderer(task=task, scene=r.scene if r is not None else None)
-                c = Configuration(
-                    task.robot.initial_configuration(),
-                    task.demo.subgoal_objects_poses[:, 0],
+                r.render_configuration(
+                    Configuration(
+                        task.robot.initial_configuration(),
+                        task.demo.subgoal_objects_poses[:, 0],
+                    )
                 )
-                r.robot[:] = c.q
-                for pose, obj in zip(c.poses, r.objects):
-                    obj.pose = pose
-                r.scene.render()
                 img = r.scene.render_image()
                 imageio.imwrite(
                     output_folder.joinpath(f"{demo_id}_{robot_pose_id}.png"), img
