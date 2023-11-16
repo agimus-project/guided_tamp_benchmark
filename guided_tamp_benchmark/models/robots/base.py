7b86b5594800eb3dd82431981d85547adf702e4c
dyymon <davkovar@email.cz>
2023-11-16 14:16:56 +0100

    moving _parse_contacts_grippers_handles function from utils.py to base.py

--- guided_tamp_benchmark/models/robots/base.py
+++ guided_tamp_benchmark/models/robots/base.py
@@ -5,13 +5,13 @@
 #     Author: David Kovar <kovarda8@fel.cvut.cz>
 
 from abc import abstractmethod
-from typing import List
+from typing import List, Tuple
+import numpy as np
 
 import pinocchio
 
 import xml.etree.ElementTree as ET
 
-from guided_tamp_benchmark.models import parse_contacts_grippers_handles
 
 
 class BaseRobot(object):
@@ -62,7 +62,7 @@ class BaseRobot(object):
          "clearance"" float}"""
         tree = ET.parse(self.srdfFilename)
         root = tree.getroot()
-        _, grippers, _ = parse_contacts_grippers_handles(
+        _, grippers, _ = self._parse_contacts_grippers_handles(
             root, contacts=False, grippers=True, handles=False
         )
         return grippers
@@ -74,3 +74,64 @@ class BaseRobot(object):
     def footprint_pos(self) -> list[float, float]:
         """Return the position of robots base"""
         return [0.0, 0.0]
+
+    def _parse_contacts_grippers_handles(
+            self, root, contacts=False, grippers=False, handles=False
+    ) -> Tuple[dict, dict, dict]:
+        """parses info from root of elementary tree xml parser of .srdf file. Returns
+        dictionary with the wanted info in following format.
+        contacts["name"] = {"link": str, "shapes": np.array},
+        handles["name"] = {"link": str, "pose": list, "clearance"" float},
+        grippers["name"] = {"link": str, "pose": list, "clearance"" float}"""
+        contact_dict, gripper_dict, handle_dict = {}, {}, {}
+        for child in root:
+            if child.tag == "contact" and contacts:
+                try:
+                    shapes = child[2].text.split()
+                    points = child[1].text.split()
+                    parsed_shapes = []
+                    while 0 < len(shapes):
+                        s = int(shapes.pop(0))
+                        shape = []
+                        while True:
+                            n = int(shapes.pop(0))
+                            shape.append(
+                                [
+                                    float(points[3 * n]),
+                                    float(points[3 * n + 1]),
+                                    float(points[3 * n + 2]),
+                                ]
+                            )
+                            s -= 1
+                            if s == 0:
+                                break
+                        parsed_shapes.append(shape)
+
+                    contact_dict[child.attrib["name"]] = {
+                        "link": child[0].attrib["name"],
+                        "shapes": np.array(parsed_shapes),
+                    }
+                except Exception:
+                    raise NameError("srdf file is missing handles!")
+            if child.tag == "gripper" and grippers:
+                try:
+                    gripper_dict[child.attrib["name"]] = {
+                        "link": child[1].attrib["name"],
+                        "pose": [float(n) for n in child[0].text.split()],
+                        "clearance": float(child.attrib["clearance"]),
+                    }
+                except Exception:
+                    raise NameError("srdf file is missing grippers!")
+            if child.tag == "handle" and handles:
+                try:
+                    handle_dict[child.attrib["name"]] = {
+                        "link": child[1].attrib["name"],
+                        "pose": [float(n) for n in child[0].text.split()],
+                        "clearance": float(child.attrib["clearance"]),
+                    }
+                except Exception:
+                    raise NameError("srdf file is mssing handles!")
+
+        return contact_dict, gripper_dict, handle_dict
+
+
